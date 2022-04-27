5c45bca043cc3bb78a8b6e20da0682967730e64a
Emil Vikström <emil.vikstrom@barium.se>
2022-04-27 14:15:46 +0200

    Add API. Fix #22

--- etc/fredag.sh
+++ etc/fredag.sh
@@ -6,8 +6,10 @@ if [ $(date +%u) -eq 5 ]; then
     echo "Set Friday to yes"
     cp yes.html index.html
     cp yes.html.gz index.html.gz
+    echo "1" > api
 else
     echo "Set Friday to nepp"
     cp nepp.html index.html
     cp nepp.html.gz index.html.gz
+    echo "0" > api
 fi
